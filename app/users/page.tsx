6f194bb9eb39eb1c59704ae05657295ea365da7b
Surasit <surasit.jame2005@gmail.com>
2026-02-28 13:29:20 +0700

    Update page.tsx

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,63 +1,59 @@
 "use client"
 import { useEffect, useState } from "react"
 
-const Todo = () => {
-    const [todos, setTodos] = useState([])
-    const url = "https://jsonplaceholder.typicode.com/todos"
-
-    useEffect(() => {
-        const fetchTodos = async () => {
-            const res = await fetch(url)
+const Todo =()=>{
+   const [todos, setTodos] = useState([])
+    const url ="https://jsonplaceholder.typicode.com/todos";
+    
+    useEffect(()=>{
+        const fetchTodo = async() =>{
+            const res = await fetch (url);
             const data = await res.json()
-            console.log(data)
-            setTodos(data.slice(0, 10)) 
-        }
-        fetchTodos()
-    }, [])
+            console.log(data) 
+            setTodos(data);
+        };
+        fetchTodo();
+    },[]);
 
     return (
         <ul>
-            {todos.map((todo) => (
-                <li key={todo.id}>
-                   {todo.title} {todo.completed }
-                </li>
-            ))}
+           {todos.map((todo)=>(<li key={todo.id}>{todo.title}</li>))}
         </ul>
     )
+
 }
 
 const User = () => {
     const [users, setUsers] = useState([])
-    const url = "https://jsonplaceholder.typicode.com/users"
+    const url ="https://jsonplaceholder.typicode.com/users";
 
-    useEffect(() => {
-        const fetchUser = async () => {
-            const res = await fetch(url)
+    useEffect(()=>{
+        const fetchUser = async() =>{
+            const res = await fetch (url);
             const data = await res.json()
-            console.log(data)
-            setUsers(data)
-        }
-        fetchUser()
-    }, [])
+            console.log(data) 
+            setUsers(data);
+        };
+        fetchUser();
+    },[]);
 
     return (
         <ul>
-            {users.map((user) => (
-                <li key={user.id}>{user.name}</li>
-            ))}
+           {users.map((user)=>(<li key={user.id}>{user.name}</li>))}
         </ul>
     )
+
 }
 
 const UserPage = () => {
   return (
     <>
-        <h2>Users</h2>
-        <User />
-        <h2>Todos</h2>
-        <Todo />
+      <h1 className="bg-green-500 text-white p-2">Users</h1>
+      <User />
+
+      <h1 className="bg-green-500 text-white p-2">Todos</h1>
+      <Todo />
     </>
   )
 }
-
 export default UserPage
